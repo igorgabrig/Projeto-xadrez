44fd7b057dd8fef5fdb2e95e1c8df7c983d1c519
Igor Gabrig <igor_gabrig@hotmail.com>
2018-05-31 03:38:47 -0300

    Iniciado Projeto de Xadrez, criada a classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using tabuleiro;//Para usar a classe Posicao, pois a mesma possui tabuleiro como namespace.
 
-namespace xadrez_console
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace xadrez_console {
+    class Program {
+        static void Main(string[] args) {
+            Posicao P = new Posicao(3,4);
+
+            Console.WriteLine("Posicao: " + P);
+
+            Console.ReadLine();
+
         }
     }
 }
