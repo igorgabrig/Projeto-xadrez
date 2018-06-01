a604a48fa6d74aa1e40400c6f7a8f6982452bedf
Igor Gabrig <igor_gabrig@hotmail.com>
2018-06-01 00:47:17 -0300

    Comecando a implementar o Tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -4,9 +4,8 @@ using tabuleiro;//Para usar a classe Posicao, pois a mesma possui tabuleiro como
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-            Posicao P = new Posicao(3,4);
 
-            Console.WriteLine("Posicao: " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
             Console.ReadLine();
 
